4dfb5cb5f7310320b3090c1183878041e9d6e9c2
annegrea <andreia.negrea@gmail.com>
2024-02-06 18:59:10 +0200

    - Add dummy data - Creat Product component - Create HomeScreen page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 
+import HomeScreen from "./pages/home/HomeScreen";
+
 import { Container } from "react-bootstrap";
 
-import './assets/styles/index.css'
-import "./assets/styles/bootstrap.custom.css"
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 
 function App() {
   return (
@@ -12,10 +14,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to CyberBoutique</h1>
+          <HomeScreen />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import Product from "../../components/product/Product.jsx";
+
+import { Row, Col } from "react-bootstrap";
+import products from "../../products.js";
+const HomeScreen = () => {
+  return (
+    <>
+      <h1> Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
