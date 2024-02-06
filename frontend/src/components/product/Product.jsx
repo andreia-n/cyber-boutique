8865366663187db7cd05ba6d274f79abf275be8a
annegrea <andreia.negrea@gmail.com>
2024-02-06 19:29:29 +0200

    Create Rating component

--- frontend/src/components/product/Product.jsx
+++ frontend/src/components/product/Product.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 import { Card } from "react-bootstrap";
+import Rating from "../rating/Rating";
 
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 py-3 rounded">
+    <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
@@ -15,6 +16,12 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews}`}
+          />
+        </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
