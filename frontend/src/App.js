9352f79e5feac64de7b10a80407a81ec701efc94
annegrea <andreia.negrea@gmail.com>
2024-02-06 11:35:17 +0200

    Created Header and Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to CyberBoutique</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import { Row, Col, Container } from "react-bootstrap";
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+  return <footer>
+    <Container>
+        <Row>
+            <Col className="text-center py-3">
+                <p>CyberBoutique &copy; {currentYear}</p>
+            </Col>
+        </Row>
+    </Container>
+  </footer>;
+};
+
+export default Footer;
