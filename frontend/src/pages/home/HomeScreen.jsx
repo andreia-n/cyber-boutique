e468f868f40a0cd6c535192bfdd496ca91a79a6b
annegrea <andreia.negrea@gmail.com>
2024-02-06 20:57:53 +0200

    Fetch data from backend

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,8 +1,20 @@
-import Product from "../../components/product/Product.jsx";
+import { useState, useEffect } from "react";
 
 import { Row, Col } from "react-bootstrap";
-import products from "../../products.js";
+
+import Product from "../../components/product/Product.jsx";
+import axios from "axios";
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1> Latest Products</h1>
